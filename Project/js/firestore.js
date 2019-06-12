bd769696778db7a947b69f2e65498a7112ddeb19
Denys Slyvka <32386916+DenysSlyvka@users.noreply.github.com>
2019-06-12 16:07:21 +0200

    Riadio & Check
    Maken van Radaiobutton questions en Checkboxquestions werkt perfect.

--- Project/js/firestore.js
+++ Project/js/firestore.js
@@ -12,26 +12,183 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+// Database aanspreekbaar maken
 var firestore = firebase.firestore();
+// Referentie naar database collection maken
+var gameRef = firestore.collection("Game");
+//Random getal genereren met als grootste het aantal vragen in Firebase Collection
+var getoondeVragen = [];
 
-const docRef = firestore.doc("/game_vragen/vraag1");
+let randomGetal = "";
 
-const vraag = document.querySelector("#vraag")
-const antwoord1 = document.querySelector("#antwoord1")
-const antwoord2 = document.querySelector("#antwoord2")
-const antwoord3 = document.querySelector("#antwoord3")
-const antwoord4 = document.querySelector("#antwoord4")
-var antwoordc;
+//Methode voor gegegevens op te halen wordt opgeroepen
+haalGegevensOp();
 
-var antwoordRadio1 = document.getElementById("antwoord1").innerText;
-var antwoordRadio2 = document.getElementById("antwoord2").innerText;
-var antwoordRadio3 = document.getElementById("antwoord3").innerText;
-var antwoordRadio4 = document.getElementById("antwoord4").innerText;
-var radioEmpty;
+function maakRandomGetal() {
+    randomGetal = "";
+    randomGetal = (Math.floor((Math.random() * 5) + 1)).toString();
+    //Random getal tonen mag later weg
+    console.log(randomGetal);
+}
 
-const outputHeader = document.querySelector("#hotDogOutput");
-const inputTextField = document.querySelector("#latestHotDogStatus");
-const controleButton = document.querySelector("#controleButton");
+//Variabele voor correct antwoord in te steken tonen
+var antwoordc = "";
+
+//Ophalen van vlognummer dat overeenkomt met het random gegenereerde getal
+//Indien het Type MCSA is worden de radaiobuttons gerenderd en alle nodige gegevens worden erin gestoken
+function haalGegevensOp() {
+    maakRandomGetal();
+    console.log(getoondeVragen);
+    if (getoondeVragen.includes(randomGetal) == false) {
+        gameRef.where("Volgnummer", "==", randomGetal.toString())
+            .get()
+            .then(function (snapshot) {
+                snapshot.forEach(function (doc) {
+                    let aantalAntwoorden = (doc.id, " => ", doc.data().aantalAntwoorden);
+
+                    getoondeVragen.push(randomGetal);
+                    if (doc.data().Type == "MCSA") {
+                        renderRadiobuttons(doc, aantalAntwoorden);
+                    } else if (doc.data().Type == "MCMA") {
+                        renderCheckboxes(doc, aantalAntwoorden);
+                    }
+
+
+                });
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    } else {
+        maakRandomGetal();
+        haalGegevensOp();
+    }
+}
+
+
+
+//Radio Buttons opvullen met antwoorden van de database
+function renderRadiobuttons(doc, aantalAntwoorden) {
+    document.getElementById("controleRadioButton").style.display = "block";
+    //console.log(aantalAntwoorden)
+    document.getElementById("vraag").innerText = doc.data().Vraag;
+    for (i = 0; i < aantalAntwoorden; i++) {
+        let labelNummer = ("labelRadio" + i).toString();
+        document.getElementById(labelNummer).style.display = "block";
+
+        //Antwoord in html is verander naar een GROTE A PLS LEES DIT STRAKS
+        let antwoordNummer = ("AntwoordRadio" + i).toString();
+
+        //console.log(doc.data().Antwoorden[i]);
+        document.getElementById(antwoordNummer).innerText = doc.data().Antwoorden[i];
+    }
+    antwoordc = doc.data().Correct;
+}
+
+//Controle van gekozen radiobutton met correct antwoord
+controleRadioButton.addEventListener("click", function () {
+    for (i = 0; i < document.quiz.group1.length; i++) {
+        var radioString = "radio" + i;
+        var spanAntwoord = "AntwoordRadio" + i;
+        if (document.getElementById(radioString).checked) {
+            var gekozenantwoord = document.getElementById(spanAntwoord).innerText;
+            if (gekozenantwoord == antwoordc) {
+                console.log("correct");
+            } else {
+                console.log("fout");
+            }
+        }
+    }
+    antwoordc = "";
+    clearVraag();
+    clearRadioButtons();
+})
+
+
+//Checkboxes opvullen met antwoorden van de database
+function renderCheckboxes(doc, aantalAntwoorden) {
+    document.getElementById("controleCheckButton").style.display = "block";
+    //console.log(aantalAntwoorden)
+    document.getElementById("vraag").innerText = doc.data().Vraag;
+    for (i = 0; i < aantalAntwoorden; i++) {
+        let labelNummer = ("labelCheck" + i).toString();
+        document.getElementById(labelNummer).style.display = "block";
+
+        //Antwoord in html is verander naar een GROTE A PLS LEES DIT STRAKS
+        let antwoordNummer = ("AntwoordCheck" + i).toString();
+
+        //console.log(doc.data().Antwoorden[i]);
+        document.getElementById(antwoordNummer).innerText = doc.data().Antwoorden[i];
+    }
+    antwoordc = doc.data().Correct;
+
+}
+
+controleCheckButton.addEventListener("click", function () {
+    var gekozenantwoord = [];
+    for (i = 0; i < document.quiz.group2.length; i++) {
+        var checkString = "check" + i;
+        var spanAntwoord = "AntwoordCheck" + i;
+        if (document.getElementById(checkString).checked === true) {
+            gekozenantwoord[i] = document.getElementById(spanAntwoord).innerText;
+            console.log(gekozenantwoord);
+        }
+    }
+    var filteredGekozenAntwoord = gekozenantwoord.filter(function (el) {
+        return el != null;
+    });
+    console.log(filteredGekozenAntwoord)
+    console.log(antwoordc);
+
+
+    if (JSON.stringify(filteredGekozenAntwoord) === JSON.stringify(antwoordc)) {
+        console.log("correct");
+    } else {
+        console.log("fout");
+    }
+
+    antwoordc = "";
+    clearVraag();
+    clearCheckboxButtons();
+
+})
+
+
+function clearVraag() {
+    document.getElementById("vraag").innerText = "";
+}
+
+function clearRadioButtons() {
+    document.getElementById("controleRadioButton").style.display = "none";
+    for (i = 0; i < document.quiz.group1.length; i++) {
+        var radioString = "radio" + i;
+        var radioLabelString = ("labelRadio" + i).toString();
+        let antwoordNummer = ("AntwoordRadio" + i).toString();
+        document.getElementById(radioString).checked = false;
+        document.getElementById(radioLabelString).style.display = "none";
+        document.getElementById(antwoordNummer).innerText = "";
+    }
+    randomGetal = "";
+    maakRandomGetal()
+    haalGegevensOp();
+}
+
+function clearCheckboxButtons() {
+    document.getElementById("controleCheckButton").style.display = "none";
+    for (i = 0; i < document.quiz.group2.length; i++) {
+        var checkString = "check" + i;
+        let labelNummer = ("labelCheck" + i).toString();
+        var spanAntwoord = "AntwoordCheck" + i;
+        document.getElementById(checkString).checked = false;
+        document.getElementById(labelNummer).style.display = "none";
+        document.getElementById(spanAntwoord).innerText = "";
+    }
+    randomGetal = "";
+    maakRandomGetal()
+    haalGegevensOp();
+}
+
+
 
 //Save Data to Firebase
 /*
@@ -59,77 +216,29 @@ loadButton.addEventListener("click", function () {
     });
 });*/
 
+//checkIfEmpty();
 
-function getData() {
-    docRef.get().then(function (doc) {
+/*
+function checkIfEmpty() {
+    if (pEmpty == "") {
+        getDataQuestion();
+    }
+
+    if (radioEmpty = true) {
+        getDataAnswers();
+    }
+}
+
+function getDataQuestion() {
+    gameRef.get().then(function (doc) {
         if (doc && doc.exists) {
             const myData = doc.data();
-            vraag.innerText = myData.vraag;
-            antwoord1.innerText = myData.antwoord1;
-            antwoord2.innerText = myData.antwoord2;
-            antwoord3.innerText = myData.antwoord3;
-            antwoord4.innerText = myData.antwoord4;
-            antwoordc = myData.antwoordc;
+            vraag.innerText = myData.Vraag;
         }
     }).catch(function (error) {
         console.log("Got an error: ", error);
     });
 }
 
-if (radioEmpty = true) {
-    getData();
-}
 
-controleButton.addEventListener("click", function () {
-
-    if (document.getElementById("radio1").checked) {
-        var gekozenantwoord = document.getElementById("antwoord1").innerText
-
-        if (gekozenantwoord == antwoordc) {
-            console.log("correct")
-        } else {
-            console.log("Fout")
-        }
-    } else if (document.getElementById("radio2").checked) {
-        var gekozenantwoord2 = document.getElementById("antwoord2").innerText
-
-        if (gekozenantwoord2 == antwoordc) {
-            console.log("correct")
-        } else {
-            console.log("Fout")
-        }
-    } else if (document.getElementById("radio3").checked) {
-        var gekozenantwoord2 = document.getElementById("antwoord3").innerText
-
-        if (gekozenantwoord2 == antwoordc) {
-            console.log("correct")
-            clearRadioButtons()
-        } else {
-            console.log("Fout")
-        }
-    } else if (document.getElementById("radio4").checked) {
-        var gekozenantwoord2 = document.getElementById("antwoord4").innerText
-
-        if (gekozenantwoord2 == antwoordc) {
-            console.log("correct")
-        } else {
-            console.log("Fout")
-        }
-    }
-})
-
-function checkRadioEmpty() {
-    if ((antwoordRadio1 = "") &&
-        (antwoordRadio2 = "") &&
-        (antwoordRadio3 = "") &&
-        (antwoordRadio4 = "")) {
-        radioEmpty = true;
-    }
-}
-
-function clearRadioButtons() {
-    antwoordRadio1 = ""
-    antwoordRadio2 = ""
-    antwoordRadio3 = ""
-    antwoordRadio4 = ""
-}
+*/
